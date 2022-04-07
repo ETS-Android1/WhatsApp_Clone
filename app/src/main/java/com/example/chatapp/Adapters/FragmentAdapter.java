cd137979962fef1ac46b6750dd3786eb2a65483d
imshukla10 <ritesh.1923ec1099@kiet.edu>
2022-04-07 21:14:05 +0530

    First Commit

---
+++ app/src/main/java/com/example/chatapp/Adapters/FragmentAdapter.java
@@ -0,0 +1,49 @@
+package com.example.chatapp.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.chatapp.Fragments.CallFragment;
+import com.example.chatapp.Fragments.ChatFragment;
+import com.example.chatapp.Fragments.StatusFragment;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new ChatFragment();
+            case 1: return new StatusFragment();
+            case 2: return new CallFragment();
+            default: return new ChatFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title=null;
+        if(position==0){
+            title="Chats";
+        }
+        if(position==1){
+            title="Status";
+        }
+        if(position==2){
+            title="Calls";
+        }
+        return title;
+    }
+}
